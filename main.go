e9f81a60f95e06532d45506f5d65376cb668c1e6
Đăng Trần <91415487+VortexDang@users.noreply.github.com>
2022-11-09 09:15:07 +0700

    Resubmit Golang-Sample-Project

--- main.go
+++ main.go
@@ -20,15 +20,19 @@ type Customer struct {
 	Contacted bool   `json:"contacted"`
 }
 
+type HomePageResponse struct {
+	Description string `json:"description"`
+	Path        string `json:"path"`
+	Method      string `json:"method"`
+}
+
 var customerList []Customer
 
+var homePageResponse []HomePageResponse
+
 func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "A brief overview of all the APIs: \n")
-	fmt.Fprintf(w, "\t Getting a single customer through a /customers/{id} path \n")
-	fmt.Fprintf(w, "\t Getting all customers through a the /customers path \n")
-	fmt.Fprintf(w, "\t Creating a customer through a /customers path \n")
-	fmt.Fprintf(w, "\t Updating a customer through a /customers/{id} path \n")
-	fmt.Fprintf(w, "\t Deleting a customer through a /customers/{id} path \n")
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(homePageResponse)
 }
 
 func getCustomers(w http.ResponseWriter, r *http.Request) {
@@ -110,6 +114,12 @@ func main() {
 	customerList = append(customerList, Customer{ID: "2", Name: "Lock", Role: "Seller", Email: "lock.huynh@jungtalens.com", Phone: "0987654321", Contacted: false})
 	customerList = append(customerList, Customer{ID: "3", Name: "James", Role: "Manager", Email: "james.vo@jungtalens.com", Phone: "1234509876", Contacted: false})
 	customerList = append(customerList, Customer{ID: "4", Name: "Han", Role: "Customer", Email: "han.nguyen@jungtalens.com", Phone: "0192837465", Contacted: true})
+
+	homePageResponse = append(homePageResponse, HomePageResponse{Description: "Getting a single customer", Path: "/customers/{id}", Method: "GET"})
+	homePageResponse = append(homePageResponse, HomePageResponse{Description: "Getting all customers", Path: "/customers", Method: "GET"})
+	homePageResponse = append(homePageResponse, HomePageResponse{Description: "Creating a customer", Path: "/customers", Method: "POST"})
+	homePageResponse = append(homePageResponse, HomePageResponse{Description: " Updating a customer", Path: "/customers/{id}", Method: "PUT"})
+	homePageResponse = append(homePageResponse, HomePageResponse{Description: " Deleting a customer", Path: "/customers/{id}", Method: "DELETE"})
 
 	r.HandleFunc("/", homePage)
 	r.HandleFunc("/customers", getCustomers).Methods("GET")
